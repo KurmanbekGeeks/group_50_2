8b448a52a79e325afa0e090dd3a3ae66afc90ea5
Kurmanbek-1 <kurmanbekmomunkulov32@gmail.com>
2025-02-01 20:54:39 +0600

    Lesson 1

---
+++ main.py
@@ -0,0 +1,41 @@
+# main.py
+from aiogram import types, Dispatcher, Bot, executor
+from decouple import config
+import logging
+
+token = config("TOKEN")
+
+bot = Bot(token=token)
+dp = Dispatcher(bot)
+
+Admins = [995712956, ]
+
+
+
+
+@dp.message_handler(commands="start")
+async def start_hanler(message: types.Message):
+    print('Обработчик старта')
+    await bot.send_message(chat_id=message.from_user.id,
+                           text=f'Hello {message.from_user.first_name}\n'
+                                f'Твой Telegram ID - {message.from_user.id}\n')
+
+    await message.answer('Привет мир')
+
+
+@dp.message_handler(commands="mem")
+async def mem_handler(message: types.Message):
+    # photo = open('media/images.jpeg', 'rb')
+
+    with open('media/images.jpeg', 'rb') as photo:
+        await bot.send_photo(chat_id=message.from_user.id,
+                             photo=photo)
+
+@dp.message_handler()
+async def echo_handler(message: types.Message):
+    await message.answer(message.text)
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    executor.start_polling(dp, skip_updates=True)
