b0063b851eea00ec89fa645cfa0e573430739b39
kurmanbek <kmomunkulov@crm.kg>
2025-02-26 20:42:18 +0600

    Lesson 7

--- main.py
+++ main.py
@@ -2,7 +2,8 @@
 from aiogram import executor
 import logging
 from handlers import (commands, echo, quiz, FSM_registration,
-                      store_fsm, send_products, delete_products)
+                      store_fsm, send_products, delete_products,
+                      edit_products)
 from config import dp, Admins, bot
 import buttons
 from db import main_db
@@ -26,6 +27,7 @@ store_fsm.register_handlers_store(dp)
 
 send_products.register_handlers(dp)
 delete_products.register_handlers(dp)
+edit_products.register_handlers(dp)
 
 # ==========================
 echo.register_handlers(dp)
